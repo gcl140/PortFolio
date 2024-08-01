cf08a801a32e3384c96ef0a73cf2f778b909a42f
gcl140 <140011591+gcl140@users.noreply.github.com>
2024-08-01 11:19:56 +0300

    Update scripts-music.js

--- scripts/scripts-music.js
+++ scripts/scripts-music.js
@@ -239,10 +239,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     toggleElements.forEach(element => {
         element.addEventListener('click', () => {
-            if (element.style.backgroundColor === 'green') {
+            if (element.style.backgroundColor === '#1db954') {
                 element.style.backgroundColor = '#333';
             } else {
-                element.style.backgroundColor = 'green';
+                element.style.backgroundColor = '#1db954';
             }
         });
     });
